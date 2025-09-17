07cbed064a92ce1e74f1d065ef9739b38ea36029
Oleksandr Vlasov <drumlife182@gmail.com>
2025-09-17 09:36:22 +0200

    feat: worked with arrays

--- week2/arrays/scores/scores.c
+++ week2/arrays/scores/scores.c
@@ -1,27 +1,55 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// const int N = 3;
+
+// float average(int length, int array[]);
+
+// int main(void)
+// {
+//     int scores[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         scores[i] = get_int("Score: ");
+//     }
+
+//     printf("Average: %f\n", average(N, scores));
+// }
+
+// float average(int length, int array[])
+// {
+//     int sum = 0;
+//     for (int i = 0; i < length; i++)
+//     {
+//         sum += array[i];
+//     }
+//     return sum / (float)length;
+// }
+
 const int N = 3;
 
-float average(int length, int array[]);
+float avrage(int length, int array[]);
 
 int main(void)
 {
+    // int score1 = 72; // binary(1001000) 4byte = 32bit
+    // int score2 = 73; // binary(1001001) 4byte = 32bit
+    // int score3 = 33; // binary(1000001) 4bytes = 32bit
+
     int scores[N];
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         scores[i] = get_int("Score: ");
     }
 
-    printf("Average: %f\n", average(N, scores));
+    printf("Average: %f\n", avrage(N, scores));
 }
 
-float average(int length, int array[])
+float avrage(int length, int array[])
 {
     int sum = 0;
     for (int i = 0; i < length; i++)
     {
         sum += array[i];
     }
-    return sum / (float)length;
+    return sum / (float) length;
 }
