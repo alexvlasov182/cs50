3eda1669e3a01ef0f50b6f9e9a6179f3731c8794
Oleksandr Vlasov <drumlife182@gmail.com>
2025-09-25 11:05:23 +0200

    docs: finished lection algorithms

---
+++ week3/iteration_recursion/iteration_recursion.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+  int height = get_int("Height: ");
+
+  draw(height);
+}
+
+
+void draw(int n)
+{
+  // Base case
+  if (n <= 0)
+  {
+    return;
+  }
+  // Print a pyramid of height n - 1
+ draw(n - 1);
+
+ // Print oen more row
+ for (int i = 0; i < n; i++)
+ {
+  printf("#");
+ }
+ printf("\n");
+}
